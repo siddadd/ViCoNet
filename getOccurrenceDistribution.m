40ea17791e363d6988cbf374c72262ce13d9bbf0
Siddharth <ska130@redrealm.cse.psu.edu>
2016-04-20 15:52:40 -0400

    .

--- getOccurrenceDistribution.m
+++ getOccurrenceDistribution.m
@@ -1,8 +1,27 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%	  														     	 %%%	
+%%%	  %%	   %%  %%   %%%%%%  %%%%%%  %%%   %%  %%%%%%  %%%%%%%%   %%%
+%%%	   %%	  %%   %%   %% 	    %%  %%  %%%%  %%  %%		 %%		 %%%
+%%%	    %%   %%	   %%   %%	    %%  %%  %% %% %%  %%%%%%	 %%      %%%
+%%%	     %% %%	   %%   %%	    %%  %%  %%  %%%%  %%		 %%		 %%%	
+%%%	      %%%      %%   %%%%%%  %%%%%%  %%  %%%%  %%%%%%     %%		 %%%
+%%%                                                               	 %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%																	 %%%			 
+%%% This function generates a distribution of occurrence weights     %%%
+%%% 06/19/2015											      		 %%% 
+%%% Siddharth Advani										      	 %%%
+%%%																	 %%%		
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% 																 %%%		
+%%% Copyright (c) 2015 Siddharth Advani                              %%%
+%%% Distributed under the MIT License                                %%%
+%%% See MITlicense.txt file in the distribution folder.              %%%
+%%%                                                                  %%%
+%%% Contact: Siddharth Advani at <ska130@cse.psu.edu>                %%%
+%%%																	 %%% 		
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function ProbDistribution = getOccurrenceDistribution(VCOM, VSOM, Key, candidate)
-% This function generates a distribution of occurrence weights
-% 06/20/2015
-% Siddharth Advani
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 sidx = strcmp(Key, candidate);
 
